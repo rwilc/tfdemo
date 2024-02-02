b9d001e5937eb9763a203487af3103438886f0cf
rwilc <ryan.wilcox@direct-tech.net>
2024-02-02 12:35:22 +0000

    preparing for demo

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "random_pet" "petname" {
 }
 
 resource "azurerm_resource_group" "myrg" {
-  name     = "rg-tst-uks-${var.myname}-${random_pet.petname}-001"
+  name     = "rg-tst-uks-tfdemo-${var.myname}-${random_pet.petname.id}-001"
   location = var.location
 }
+
